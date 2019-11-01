1102e0a96e26be0ad6dc30d8321ee6d36c09087e
Andre Carnieletto Dotto <280240B@curtin.edu.au>
2019-11-01 14:23:39 +0800

    update modelling

--- R/modelling.R
+++ R/modelling.R
@@ -1,30 +1,101 @@
-library(shiny)
-library(rdrop2)
-library(dplyr)
-library(readr)
+library(Cubist)
+library(plsr)
 library(mxnet)
 library(caret)
-## 
 
 
-control <- caret::trainControl(method = "cv", savePredictions = TRUE, verboseIter = TRUE, number=5)
-tune.pls <- caret::train(variable~., data=prepro[[i]], method='pls', tuneLength= 5, trControl = control, verbose=TRUE)
-return(tune.pls)
-selected.pls.c <- tune.pls$bestTune$ncomp
-# calibrate the model
-mod <- pls::plsr(variable~., data=prepro[[i]], ncomp= tune.pls$bestTune$ncomp, method="simpls", validation="none") 
+#select the imposted spectra
+spectra <- rslocal 
 
-#cubist
-control <- trainControl(method="optimism_boot", number=10)
 
-fit.cubist <- train(variable~.,data=gssl.imported, 
-                    method="cubist",
-                    trControl=control)
-print(fit.cubist)
-beep(2)
-results.cubist <- getTrainPerf(fit.cubist)
-results.cubist
+# create list with all reflectance
+pp <- function(spectra, option){
+  
+  if (any(is.na(spectra[-1]))) {
+    warning("Check for NAs!")
+  }
+  # reflectance
+  if (option == "REFLECTANCE") spectra.pp <- spectra
+  # absorbance
+  if (option == "ABSORBANCE") spectra.pp <- cbind(spectra[1], log10(1/spectra[-1]))
+  # SG Derivative
+  if (option == "SGDERIVATIVE") spectra.pp <- cbind(spectra[1], data.frame(prospectr::savitzkyGolay(spectra[-1], p = 2, w = 9, m = 1)))
+  # SNV
+  if (option == "SNV") spectra.pp <- cbind(spectra[1], prospectr::standardNormalVariate(X = spectra[-1])) 
+  # absorbance + SNV
+  if (option == "ABS-SNV") spectra.pp <- cbind(spectra[1], prospectr::standardNormalVariate(X = abs)) 
+  # absorbance + derivative
+  if (option == "ABS-SGDERIVATIVE") spectra.pp <- cbind(spectra[1], prospectr::standardNormalVariate(X = der))
+  #SG smoothing 5
+  if (option == "SGSMOOTH5") spectra.pp <- cbind(spectra[1], data.frame(prospectr::savitzkyGolay(spectra[-1], p = 2, w = 5,  m = 0)))
+  #SG smoothing 15
+  if (option == "SGSMOOTH15") spectra.pp <- cbind(spectra[1], data.frame(prospectr::savitzkyGolay(spectra[-1], p = 2, w = 15, m = 0)))
+  #SG smoothing 29
+  if (option == "SGSMOOTH29") spectra.pp <- cbind(spectra[1], data.frame(prospectr::savitzkyGolay(spectra[-1], p = 2, w = 29, m = 0)))
+  return(spectra.pp)
+}
 
+# option for preprocessing options
+prepro <- c("REFLECTANCE", 
+            "ABSORBANCE", 
+            "SGDERIVATIVE", 
+            "SNV", 
+            "ABS-SNV", 
+            "ABS-SGDERIVATIVE", 
+            "SGSMOOTH5", 
+            "SGSMOOTH15", 
+            "SGSMOOTH29") 
+
+# list of the model options
+models <- c("pls","cubist") 
+# create null output
+output <- NULL
+
+# Selecting the best model and preprocessing
+set.seed(123)
+
+# option 1  
+for (p in prepro) { # 9 preprocessing
+  for (m in models) { # 2 models
+    print(paste0("Running ", p, " and ", m))
+    pp.s <- pp(spectra = spectra, option = prepro)
+    control  <- caret::trainControl(method = "cv", savePredictions = TRUE, verboseIter = TRUE, number=2, allowParallel = FALSE)
+    tune.pls <- caret::train(y=pp.s[ ,1], x=pp.s[,-1], method= m, tuneLength= 5, trControl = control, verbose=TRUE)
+    stat <- sm_Stats(tune.pls$pred$pred, tune.pls$pred$obs, plot = F)
+    rownames(stat) <- paste0(p, "-", m)
+    print(stat)
+    if (is.null(output)){
+      output <- stat
+    } else { 
+      if (output[1] < stat[1])
+        output <- stat
+      print(output)
+    }
+  } #close m
+}#close p
+
+
+
+# option 2
+library(foreach)
+library(doParallel)
+cl <- makeCluster(detectCores() - 1)
+registerDoParallel(cl)
+alloptions <- expand.grid(pp = prepro, model = models)
+model.pp <- foreach(k = 1:nrow(alloptions), .combine = rbind) %dopar% {
+  p <- as.character(alloptions[k, ]$pp)
+  m <- as.character(alloptions[k, ]$model)
+  # check 'invisible'
+  pp.s <- pp(spectra = spectra, option = prepro)
+  control  <- caret::trainControl(method = "cv", savePredictions = TRUE, verboseIter = TRUE, number=10, allowParallel = T)
+  tune.pls <- caret::train(y=pp.s[ ,1], x=pp.s[,-1], method= m, tuneLength= 15, trControl = control, verbose=TRUE)
+  stat <- sm_Stats(tune.pls$pred$pred, tune.pls$pred$obs, plot = F)
+  rownames(stat) <- paste0(p, "-", m)
+  print(stat)
+  
+}
+model.pp
+stopCluster(cl)
 
 
 
